edc3717965ce3cf25ed576017ed7bbfbd502f71c
prateek <prateekvermasdg007@gmail.com>
2021-08-08 20:09:44 +0530

    add print all pairs

--- src/com/learnDsa/array/BasicArrayManipulation.java
+++ src/com/learnDsa/array/BasicArrayManipulation.java
@@ -1,15 +1,23 @@
 package com.learnDsa.array;
 
 public class BasicArrayManipulation {
-    public BasicArrayManipulation(){
+    public BasicArrayManipulation() {
 
     }
 
-    public static void reverseArray(int[] arr){
-        int startIndex=0;
-        int endIndex=arr.length-1;
-        while (startIndex<endIndex){
-            arr[startIndex]  = arr[startIndex] + arr[endIndex];
+    public static void printAllPairs(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                System.out.println("( " + arr[i] + " , " + arr[j] + " )");
+            }
+        }
+    }
+
+    public static void reverseArray(int[] arr) {
+        int startIndex = 0;
+        int endIndex = arr.length - 1;
+        while (startIndex < endIndex) {
+            arr[startIndex] = arr[startIndex] + arr[endIndex];
             arr[endIndex] = arr[startIndex] - arr[endIndex];
             arr[startIndex] = arr[startIndex] - arr[endIndex];
             startIndex++;
