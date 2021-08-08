f92f32d260888b710227bdd727d0a38764a02929
prateek <prateekvermasdg007@gmail.com>
2021-08-08 17:44:32 +0530

    passed the array by reference

---
+++ src/com/learnDsa/array/PassByReference.java
@@ -0,0 +1,18 @@
+package com.learnDsa.array;
+
+public class PassByReference {
+    public PassByReference(){
+
+    }
+
+    public static void passArray(){
+        Integer[] marks = new Integer[] {20,30,40 };
+        PassByReference.printArray(marks);
+    }
+
+    public static void printArray(Integer[] array){
+        for(Integer x : array){
+            System.out.println(x.intValue());
+        }
+    }
+}
