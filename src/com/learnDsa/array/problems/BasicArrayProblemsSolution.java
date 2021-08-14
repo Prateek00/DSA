6e090d2ebc04e4bb94ee0da71f83d505c2feba96
prateek <prateekvermasdg007@gmail.com>
2021-08-14 16:34:53 +0530

    add lower bound

---
+++ src/com/learnDsa/array/problems/BasicArrayProblemsSolution.java
@@ -0,0 +1,30 @@
+package com.learnDsa.array.problems;
+
+import java.util.Vector;
+
+public class BasicArrayProblemsSolution {
+
+    // array is sorted, using binary sort
+    public static int getlowerBound(int[] arr, int number) {
+        int low=0;
+        int high=arr.length;
+        int mid;
+        while(low<=high){
+            mid = (low+high)/2;
+            if(arr[mid]==number)
+                return number;
+            else if(arr[mid]>number)
+                high=mid-1;
+            else
+                low = mid+1;
+
+        }
+
+        //case when lower bound doesn't exist, return -1
+        if(low==0)
+            return -1;
+        else
+            return arr[high];
+
+    }
+}
