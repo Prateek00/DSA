ebceec6571415fedf609fc981ce5b16f21add198
prateek <prateekvermasdg007@gmail.com>
2021-08-15 17:15:05 +0530

    add Expo

--- src/com/learnDsa/bitManipulation/BasicBitManipulations.java
+++ src/com/learnDsa/bitManipulation/BasicBitManipulations.java
@@ -10,11 +10,35 @@ public class BasicBitManipulations {
     // iterating each bit O(log n)
     public static int getSetBitsCount(int key){
         int count=0;
-        while(key != 0){
+        while(key > 0){
             count+=(key&1);
             key = key>>1;
         }
         return count;
     }
+
+    // iterating to number of set bits, O(1), max 32 times.
+    public static int getSetBitsCountByAndOp(int key){
+        int ans=0;
+        while (key >0){
+            key = key & (key-1);
+            ans++;
+        }
+        return ans;
+    }
+
+    public static int getExpo(int base,int expo){
+        int ans = 1;
+        while (expo>0){
+            int last_bit=expo&1;
+            if(last_bit==1){
+                ans*=base;
+            }
+            base = base*base;
+            expo=expo>>1;
+        }
+        return ans;
+    }
+
 
 }
