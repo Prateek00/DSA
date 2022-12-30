44efc9854073b78b6dc658a87277739c85999d59
Prateek <prateekvermasdg007@gmail.com>
2022-12-31 00:17:28 +0530

    added basic sliding window problems

---
+++ src/learning/learnDSA/slidingWindow/BasicSlidingWindowProblem.java
@@ -0,0 +1,25 @@
+package learning.learnDSA.slidingWindow;
+
+public class BasicSlidingWindowProblem {
+    //https://leetcode.com/problems/longest-substring-without-repeating-characters/
+    public int lengthOfLongestSubstring(String s) {
+        int i=0,j=0;
+        int result=0;
+        int currentLength;
+        int len=s.length();
+        while(j<len){
+            for(int k=i;k<j;k++){
+                if(s.charAt(k)==s.charAt(j)){
+                    i=k+1;
+                    break;
+                }
+            }
+            currentLength=j-i+1;
+            if(currentLength>result){
+                result=currentLength;
+            }
+            j++;
+        }
+        return result;
+    }
+}
